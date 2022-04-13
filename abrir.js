5f6c3b49bf52c5eccde36f950f5bca07cb6242db
VitorRekobe <98287250+VitorRekobe@users.noreply.github.com>
2022-04-13 15:03:10 -0300

    Add files via upload

---
+++ abrir.js
@@ -0,0 +1,10 @@
+function abrir(){
+    var div = document.getElementById("MNBG");
+    
+    if(div.style.display == "block"){
+        div.style.display = "none";
+    } else {
+        div.style.display = "block";
+    }
+
+}
